583ca91c6df2d104e54596f4726e49a2979f0a66
Łukasz Sobocinski <l.sobocinski123@gmail.com>
2023-11-10 15:21:27 +0100

    Update scripts

--- slurm_scripts/centralized_training.sh
+++ slurm_scripts/centralized_training.sh
@@ -17,37 +17,29 @@
 base_dir=/home/cir/lsobocinski
 venv_path=${base_dir}/meningioma_dl/venv1
 
-n_workers=1
-n_epochs=100
-
 n_trials=1
-augmentation_config="${1:-exp_8_005_augment_prob}"
-hyperparameters_config_name="${2:-0002_lr_09_gamma}"
-scheduler_name="${4:-exponent}"
-preprocessing_settings_name="${5:-default}"
-study_name="${3:-playground}_${augmentation_config}_${hyperparameters_config_name}_${preprocessing_settings_name}"
-resnet_layers_to_unfreeze="${6:-0}"
-use_training_data_for_validation="${7:-False}"
-loss_function_name="${8:-cross_entropy}"
+augmentation_specs="${1:-basic_01p}"
+scheduler_specs="${2:-05_lr_099_gamma}"
+preprocessing_specs="${3:-no_resize}"
+training_specs="${4:-central_2_epochs}"
+run_id="${5:-playground}_${preprocessing_specs}_${augmentation_specs}_${scheduler_specs}_${SLURM_JOBID}"
 
 module add Python/3.7.3-foss-2019a
 module add PyTorch/1.6.0-foss-2019a-Python-3.7.3
 # virtualenv --system-site-packages ${base_dir}/meningioma_dl/venv
 source ${venv_path}/bin/activate
 
-echo "Running Slurm job with id $SLURM_JOBID and name ${study_name}_${SLURM_JOBID} "
+echo "Running Slurm job with id $SLURM_JOBID and name $run_id"
 echo "venv path: $venv_path"
 
 #lddpython is needed to load a newer glibc
 ${base_dir}/meningioma_dl/slurm_scripts/lddpython ${base_dir}/meningioma_dl/meningioma_dl/run_optuna_study.py \
   --device_name="cuda" \
-  --n_workers=${n_workers} \
-  --env_file_path=${base_dir}/meningioma_dl/envs/slurm.env --n_epochs="${n_epochs}" \
-  --n_trials="${n_trials}" --study_name="${study_name}" --run_id="${SLURM_JOBID}" \
-  --batch_size=4 --validation_interval=4 --search_space_name="${augmentation_config}" \
-  --hyperparameters_config_name="${hyperparameters_config_name}" \
-  --scheduler_name="${scheduler_name}" \
-  --preprocessing_settings_name="${preprocessing_settings_name}" \
-  --resnet_layers_to_unfreeze="${resnet_layers_to_unfreeze}" \
-  --use_training_data_for_validation="${use_training_data_for_validation}" \
-  --loss_function_name="${loss_function_name}"
+  --env_file_path=${base_dir}/meningioma_dl/envs/slurm.env \
+  --n_trials=$n_trials \
+  --run_id="${run_id}" \
+  --validation_interval=4 \
+  --augmentations_specs_name="${augmentation_specs}" \
+  --scheduler_specs_name="${scheduler_specs}" \
+  --preprocessing_specs_name="${preprocessing_specs}" \
+  --training_specs_name="${training_specs}"
--- slurm_scripts/run_grid_search.sh
+++ slurm_scripts/run_grid_search.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
 slurm_script_path=/home/cir/lsobocinski/meningioma_dl/slurm_scripts/centralized_training.sh
-# augmentation_configs=("exp_9_1_01_augment_prob" "exp_9_1_025_augment_prob")
-# hyperparameters_configs=("0001_lr_0999_gamma" "002_lr_099_gamma" "0005_lr_0995_gamma")
-# hyperparameters_configs=("cosine_lr_0003_t0_20" "cosine_lr_0002_t0_40")
 
-augmentation_configs=("no_augmentation")
-# hyperparameters_configs=("0001_lr_0999_gamma" "002_lr_099_gamma" "0005_lr_0995_gamma")
+augmentation_specs=("basic_01p")
+scheduler_specs=("05_lr_099_gamma")
+preprocessing_specs=("no_resize")
+training_settings=("central_200_epochs")
 
-hyperparameters_configs=("1_lr_099_gamma")
-scheduler_name="exponent"
-preprocessing_settings=("no_padding")
-resnet_layers_to_unfreeze=(1 2 3 4)
-use_training_data_for_validation="True"
-loss_function_name="cross_entropy"
+runs_main_name="1"
 
-runs_main_name="unfreeze_${resnet_layers_to_unfreeze}"
-
-for augmentation_config in "${augmentation_configs[@]}"; do
-    for hyperparameters_config in "${hyperparameters_configs[@]}"; do
-        for preprocessing_setting in "${preprocessing_settings[@]}"; do
-            for layer in "${resnet_layers_to_unfreeze[@]}"; do
-                sbatch -p full --qos jobarray "$slurm_script_path" "$augmentation_config" \
-                "$hyperparameters_config" "$runs_main_name" "$scheduler_name" "$preprocessing_setting" \
-                "$layer" "$use_training_data_for_validation" "$loss_function_name"
+for augmentation in "${augmentation_specs[@]}"; do
+    for scheduler in "${scheduler_specs[@]}"; do
+        for preprocessing in "${preprocessing_specs[@]}"; do
+            for training in "${training_settings[@]}"; do
+                sbatch -p full --qos jobarray "$slurm_script_path" \
+                  "$augmentation" \
+                  "$scheduler" \
+                  "$preprocessing" \
+                  "$training" \
+                  "$runs_main_name"
                 sleep 1m
             done
         done
