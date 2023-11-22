c61d7e26fcb7a800833f9132dde985c25d83f578
Łukasz Sobocinski <l.sobocinski123@gmail.com>
2023-11-22 20:45:37 +0100

    Delete unnecessary specs

--- meningioma_dl/experiments_specs/model_specs.py
+++ meningioma_dl/experiments_specs/model_specs.py
@@ -29,22 +29,6 @@ MODELS = {
         "resnet_shortcut_type": "B",
         "number_of_layers_to_unfreeze": 4,
     },
-    "class_1_and_2_together_0_unfreezed": {
-        "model_depth": 10,
-        "resnet_shortcut_type": "B",
-        "number_of_layers_to_unfreeze": 0,
-        "number_of_classes": 2,
-        "class_mapping": {1: 1, 2: 1, 3: 2},
-        "evaluation_metric_weighting": "macro",
-    },
-    "class_2_and_3_together_0_unfreezed": {
-        "model_depth": 10,
-        "resnet_shortcut_type": "B",
-        "number_of_layers_to_unfreeze": 0,
-        "number_of_classes": 2,
-        "class_mapping": {1: 1, 2: 2, 3: 2},
-        "evaluation_metric_weighting": "macro",
-    },
     "class_1_and_2_together_2_unfreezed": {
         "model_depth": 10,
         "resnet_shortcut_type": "B",
