e5f4e9affd19392cbef5f94da7be4e772db91689
lsobocinski <lukasz.p.sobocinski@gmail.com>
2024-05-20 14:33:07 +0200

    Add stronger augmentation specs

--- meningioma_dl/experiments_specs/augmentation_specs.py
+++ meningioma_dl/experiments_specs/augmentation_specs.py
@@ -57,6 +57,57 @@ def get_basic_augmentation(
     return transformations
 
 
+def get_strong_augmentation(
+    probability_base: float, mask_after_gaussian: bool = True, gaussian_std:float = 0.15
+) -> List[transforms.Transform]:
+    affined_translate = 15
+    transformations = [
+        transforms.RandStdShiftIntensityd(
+            keys=["img", "mask"],
+            prob=probability_base + 0.05,
+            factors=0.2,
+        ),
+        transforms.RandGaussianNoised(
+            keys=["img"],
+            std=gaussian_std,
+            prob=probability_base,
+        ),
+    ]
+    if mask_after_gaussian:
+        transformations.append(transforms.MaskIntensityd(keys=["img"], mask_key="mask"))
+    transformations.extend(
+        [
+            transforms.RandRotated(
+                keys=["img", "mask"],
+                prob=probability_base + 0.1,
+                range_x=math.pi / 4,
+                range_y=math.pi / 4,
+                range_z=math.pi / 4,
+                padding_mode="zeros",
+                keep_size=False,
+            ),
+            transforms.RandZoomd(
+                keys=["img", "mask"],
+                min_zoom=0.8,
+                max_zoom=1.2,
+                prob=probability_base + 0.05,
+                keep_size=False,
+            ),
+            transforms.RandAffined(
+                keys=["img", "mask"],
+                prob=probability_base + 0.05,
+                translate_range=[
+                    (-affined_translate, affined_translate),
+                    (-affined_translate, affined_translate),
+                    (-affined_translate, affined_translate),
+                ],
+                padding_mode="zeros",
+            ),
+        ]
+    )
+    return transformations
+
+
 AUGMENTATIONS = {
     "no_augment": [],
     "basic_01p": get_basic_augmentation(0.1),
@@ -64,6 +115,12 @@ AUGMENTATIONS = {
     "basic_03p": get_basic_augmentation(0.3),
     "basic_05p": get_basic_augmentation(0.5),
     "basic_08p": get_basic_augmentation(0.8),
+    "basic_1p": get_basic_augmentation(1.0),
+    "strong_05p": get_strong_augmentation(0.5),
+    "strong_08p":get_strong_augmentation(0.8),
+    "strong_less_gaussian_05p": get_strong_augmentation(0.5, gaussian_std=0.1),
+    "strong_less_gaussian_08p": get_strong_augmentation(0.8, gaussian_std=0.1),
+    "strong_less_gaussian_1p":get_strong_augmentation(1.0, gaussian_std=0.1),
     "basic_02p_no_mask_after_gaussian": get_basic_augmentation(0.2, False),
 }
 
