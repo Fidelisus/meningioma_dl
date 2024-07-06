3823ff8275c29b8a880bb86a7c7ea091302919ec
Łukasz Sobocinski <l.sobocinski123@gmail.com>
2024-07-06 23:28:48 +0200

    Add ensemble_id to evaluation script

--- slurm_scripts/run_evaluation_slurm.sh
+++ slurm_scripts/run_evaluation_slurm.sh
@@ -1,5 +1,6 @@
 use_test_data="True"
 fold=4
+ensemble_id="False"
 
 module add "Python/3.9.5-GCCcore-8.2.0"
 module add "PyTorch/1.9.0-foss-2019a"
@@ -8,5 +9,5 @@ module add "PyTorch/1.9.0-foss-2019a"
 for run_id in "$@"
 do
     sbatch -p centos7 --qos normal  \
- /home/cir/lsobocinski/meningioma_dl/slurm_scripts/run_evaluation.sh "$run_id" "$use_test_data" "$fold"
+ /home/cir/lsobocinski/meningioma_dl/slurm_scripts/run_evaluation.sh "$run_id" "$use_test_data" "$fold" "$ensemble_id"
 done
