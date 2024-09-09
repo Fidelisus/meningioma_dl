7476c17a9c969124fc011fdf3b6f5fb32af285c7
lsobocinski <lukasz.p.sobocinski@gmail.com>
2024-09-09 22:14:18 +0200

    Update the evaluation script

--- slurm_scripts/run_evaluation.sh
+++ slurm_scripts/run_evaluation.sh
@@ -10,14 +10,14 @@
 #SBATCH --time=0:30:00
 #SBATCH --job-name=meningioma_classification
 
-base_dir=/home/cir/lsobocinski/meningioma_dl
+base_dir=/home/cir/lsobocinski/meningioma_dl/
 
 training_run_id="$1"
 use_test_data="$2"
 cv_fold="$3"
 ensemble_id="$4"
 if [ "$ensemble_id" = "False" ]; then
-   trained_model_path="${base_dir}/data/meningioma/optuna/trials/models/$training_run_id/epoch_-1.pth.tar"
+   trained_model_path="/home/cir/lsobocinski/data/meningioma/optuna/trials/models/$training_run_id/epoch_-1.pth.tar"
    script_name="evaluate.py"
 else
    trained_model_path="$ensemble_id"
@@ -26,15 +26,15 @@ fi
 echo "Trained model path: $trained_model_path"
 
 preprocessing_specs="no_resize"
-#model_specs="class_2_and_3_together_3_unfreezed"
-model_specs="resnet_10_4_unfreezed"
+model_specs="class_2_and_3_together_4_unfreezed"
+#model_specs="resnet_10_4_unfreezed"
 run_id="eval_${training_run_id}_${SLURM_JOBID}"
 
 module add cuDNN/7.6.5.32-CUDA-10.1.243 > /dev/null 2>&1
 
 echo "Running Slurm job with id $SLURM_JOBID and name $run_id"
 
-singularity exec --nv ${base_dir}image.sif ${base_dir}entrypoint.sh ${base_dir}/meningioma_dl/"$script_name" \
+singularity exec --nv ${base_dir}image.sif ${base_dir}entrypoint.sh ${base_dir}meningioma_dl/"$script_name" \
   "$trained_model_path" \
   --env_file_path=${base_dir}/envs/slurm.env \
   --run_id="${run_id}" \
--- slurm_scripts/run_evaluation_slurm.sh
+++ slurm_scripts/run_evaluation_slurm.sh
@@ -1,6 +1,6 @@
 use_test_data="True"
 fold=4
-ensemble_id="centralized_3_classes"
+ensemble_id="False"
 
 module add "Python/3.9.5-GCCcore-8.2.0"
 module add "PyTorch/1.9.0-foss-2019a"
@@ -8,6 +8,6 @@ module add "PyTorch/1.9.0-foss-2019a"
 # --nodelist=on5
 for run_id in "$@"
 do
-    sbatch -p centos7 --qos normal  \
+    sbatch -p centos7 --qos jobarray  \
  /home/cir/lsobocinski/meningioma_dl/slurm_scripts/run_evaluation.sh "$run_id" "$use_test_data" "$fold" "$ensemble_id"
 done
